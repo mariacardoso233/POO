f41a785c65176bbe6631d40b5132ab170b14bcc1
mariacardoso233 <mariacardoso233@gmail.com>
2020-06-11 16:18:57 +0100

    new

--- F03 - DOM/EX5.js
+++ F03 - DOM/EX5.js
@@ -1,34 +1,44 @@
-const fromGame = document.querySelector("form");
+const fromGame = document.querySelector('form');
 
-fromGame.addEventListener("submit", function (event) {
+//Alinea C)
+
+fromGame.addEventListener("submit", function(event) {
     const txtGameYear = document.querySelector("#txtGameYear").value;
 
     if (txtGameYear > 1950 && txtGameYear <= new Date().getFullYear()) {
-        alert("Validação OK!!");
+        alert('Validação OK!');
 
-        const table = document.querySelector("table")
-        const gameName = document.querySelector("#txtGameName").value;
-        const gameYear = document.querySelector("#txtGameYear").value;
-        const gameGenre = document.querySelector("#txtGameGenre").value;
-        const gameCover = document.querySelector("#urlGameCover").value;
+        const table = document.querySelector('table');
+        const gameName = document.querySelector('#txtGameName').value;
+        const gameYear = document.querySelector('#txtGameYear').value;
+        const gameGenre = document.querySelector('#sltGameGenre').value;
+        const gameCover = document.querySelector('#urlGameCover').value;
 
-        table.innerHTML += `
-        <tr>
-            <td>${gameName}</td>
-            <td>${gameYear}</td>
-            <td>${gameGenre}</td>
-            <td><img src="${gameCover}"/></td>
-            <td><input type="button" value="x"></td>
-        </tr>
-        `
-        /*x.addEventListener("click", function() {
-            this.parentNode.parentNode.remove();
-        });*/
+        let line = document.createElement('tr');
+        line.innerHTML = `
+            <tr>
+                <td>${gameName}</td>
+                <td>${gameYear}</td>
+                <td>${gameGenre}</td>
+                <td><img src="${gameCover}"></td>
+                <td><input type="button" value="x"></td>
+            </tr>
+        `;
+
+        table.append(line);
+
+        buttonX = line.querySelector('input')[0];
+        buttonX.addEventListener('click', function() {
+            this.parentNone.parentNone.remove();
+        });
+
+
 
     } else {
         alert("Erro!!");
     }
 
+    formGame.reset();
     event.preventDefault();
 
 });
