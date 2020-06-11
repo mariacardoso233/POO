adbd3a5b15cd3630abac0c7c0e1ee88db9c8c77e
mariacardoso233 <mariacardoso233@gmail.com>
2020-06-12 00:59:05 +0100

    video

--- F06_BandsApp/js/app.js
+++ F06_BandsApp/js/app.js
@@ -51,49 +51,56 @@ class App {
                 name: 'iKON',
                 genre: 'Hip-Hop',
                 photo: 'https://c-sf.smule.com/sf/s77/arr/7d/ea/de26a1fd-9574-452a-9cd9-38e3cd04d9a3_256.jpg',
-                description: 'Genre: K-pop, Hip-hop // Debut: 2015// Label: YG Entertainment // Members: B.I, Jinhwan, Yunhyeong, Bobby, Donghyuk, Junhoe, Chanwoo'
+                description: 'Genre: K-pop, Hip-hop // Debut: 2015// Label: YG Entertainment // Members: B.I, Jinhwan, Yunhyeong, Bobby, Donghyuk, Junhoe, Chanwoo',
+                video: ''
             },
             {
                 id: 2,
                 name: 'BTS',
                 genre: 'K-Pop',
                 photo: 'https://pbs.twimg.com/profile_images/828729925217746945/AgMZdPiL.jpg',
-                description: 'Debut: 2013 // Label: Big Hit Entertainment // Members: Jin, Suga, J-Hope, RM, Jimin, V, Jungkook'
+                description: 'Debut: 2013 // Label: Big Hit Entertainment // Members: Jin, Suga, J-Hope, RM, Jimin, V, Jungkook',
+                video: ''
             },
             {
                 id: 3,
                 name: 'GOT7',
                 genre: 'K-pop',
                 photo: 'https://cdn130.picsart.com/321294967494201.jpg?type=webp&to=crop&r=256',
-                description: 'Debut: 2014 // Label: JYP Entertainment // Members: Mark, JB, Jackson, Jinyoung, Youngjae, BamBam, Yugyeom'
+                description: 'Debut: 2014 // Label: JYP Entertainment // Members: Mark, JB, Jackson, Jinyoung, Youngjae, BamBam, Yugyeom',
+                video: ''
             },
             {
                 id: 4,
                 name: 'NCT 127',
                 genre: 'Hip-Hop',
                 photo: 'https://c-sf.smule.com/rs-s95/arr/c9/6e/c06d079f-d19d-406d-9cd9-9001382993a7.jpg',
-                description: 'Genre: k-pop, hip-hop // Debut: 2016 // Label: SM // Members: Taeil, Johnny, Taeyong, Yuta, Doyoung, Jaehyun, Winwin, Jungwoo, Mark, Haechan'
+                description: 'Genre: k-pop, hip-hop // Debut: 2016 // Label: SM // Members: Taeil, Johnny, Taeyong, Yuta, Doyoung, Jaehyun, Winwin, Jungwoo, Mark, Haechan',
+                video: ''
             },
             {
                 id: 5,
                 name: 'PLT',
                 genre: 'R&B',
                 photo: 'https://c-sf.smule.com/rs-s79/arr/a6/22/336af388-048b-4a97-bb4d-31a078191e48.jpg',
-                description: 'Genre: r&b, hip-hop // Debut: 2018 // Label: Planetarium Record // Mambers: June, Moti, Jung Jinwoo, Gaho, Villain and Kei.G'
+                description: 'Genre: r&b, hip-hop // Debut: 2018 // Label: Planetarium Record // Mambers: June, Moti, Jung Jinwoo, Gaho, Villain and Kei.G',
+                video: ''
             },
             {
                 id: 6,
                 name: 'ATEEZ',
                 genre: 'K-pop',
                 photo: 'https://pm1.narvii.com/7273/37d692975f291bce2f71244746ec61c61413cce8r1-634-634v2_00.jpg',
-                description: 'Genre: K-pop, J-pop // Debut: 2018 // Label: KQ Entertainment // Mambers: Hongjoong, Seonghwa, Yunho, Yeosang, San, Mingi, Wooyoung, and Jongho '
+                description: 'Genre: K-pop, J-pop // Debut: 2018 // Label: KQ Entertainment // Mambers: Hongjoong, Seonghwa, Yunho, Yeosang, San, Mingi, Wooyoung, and Jongho ',
+                video: ''
             },
             {
                 id: 7,
                 name: 'BIGBANG',
                 genre: 'K-pop',
                 photo: 'https://kprofiles.com/wp-content/uploads/BigBang.jpg',
-                description: 'Genre: K-pop, J-pop, hip-hop, dance electronic, R&B // Debut: 2006 // Label: YG Entertainment // Mambers: T.O.P, Taeyang, G-Dragon, Daesung, Seungri (ex-member)'
+                description: 'Genre: K-pop, J-pop, hip-hop, dance electronic, R&B // Debut: 2006 // Label: YG Entertainment // Mambers: T.O.P, Taeyang, G-Dragon, Daesung, Seungri (ex-member)',
+                video: ''
             },
         ];
 
